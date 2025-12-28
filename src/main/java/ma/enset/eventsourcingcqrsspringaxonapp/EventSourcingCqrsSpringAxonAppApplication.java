40ea0e111db2c1d2ad6cc5e61acdf0570c8d03ec
Khadija Amechate <amchatekhadija@gmail.com>
2025-12-28 23:15:05 +0100

    first commit

---
+++ src/main/java/ma/enset/eventsourcingcqrsspringaxonapp/EventSourcingCqrsSpringAxonAppApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.eventsourcingcqrsspringaxonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingCqrsSpringAxonAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSourcingCqrsSpringAxonAppApplication.class, args);
+    }
+
+}
